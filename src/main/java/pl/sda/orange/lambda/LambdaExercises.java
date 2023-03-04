9f4a6123df99645857e1a9b4b0b1fc2167ce1415
Julia Be <juliabaraniecka@gmail.com>
2023-03-04 13:37:38 +0100

    [add] - interface with implementation - polymorphism

--- src/main/java/pl/sda/orange/lambda/LambdaExercises.java
+++ src/main/java/pl/sda/orange/lambda/LambdaExercises.java
@@ -4,5 +4,12 @@ public class LambdaExercises {
 
     public static void main(String[] args) {
 
+        BoysDontCry boy = new ChlopakiNiePlacza(); //przykład polimorfizmu -> przypinam klasę-dziecko do interfejsu-rodzica
+        boy.silnoreki();
+
+        ((ChlopakiNiePlacza)boy).sing(); //rzutowanie - interface BoysDontCry nie ma takiej metody, nie mogę się dostać bezpośrednio
+
+        ChlopakiNiePlacza boy2 = new ChlopakiNiePlacza();
+        boy2.sing();
     }
 }
