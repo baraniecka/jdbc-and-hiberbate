4c392adfd799d8f358d58e6ecb250cf2929233e8
Julia Be <juliabaraniecka@gmail.com>
2023-03-04 14:44:27 +0100

    [add] - using @FunctionalInterface and describing purpose
    1) @FunctionalInterface annotation
    2) Default methods in interfaces

--- src/main/java/pl/sda/orange/lambda/BoysDontCry.java
+++ src/main/java/pl/sda/orange/lambda/BoysDontCry.java
@@ -1,7 +1,11 @@
 package pl.sda.orange.lambda;
 
+@FunctionalInterface
 public interface BoysDontCry {
 
     //functional interface because it has only one abstract method
     void silnoreki();
+
+    //metoda domyślna nie jest abstrakcyjna - ma ciało
+    default void cry() {}
 }
