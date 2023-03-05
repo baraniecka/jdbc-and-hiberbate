fa3bc0a07671fc09524b5dae178c00d2eb711fbc
Julia Be <juliabaraniecka@gmail.com>
2023-03-05 14:50:34 +0100

    [add] - h2 connection parameters

---
+++ src/main/java/pl/sda/orange/jdbc/config/H2Config.java
@@ -0,0 +1,7 @@
+package pl.sda.orange.jdbc.config;
+
+public class H2Config {
+    public final static String DB_URL = "jdbc:h2:~/databases/jdbc";
+    public final static String USER = "sa";
+    public final static String PASSWORD = "sa";
+}
