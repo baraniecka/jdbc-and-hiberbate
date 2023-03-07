a04cf3b2a7eb960dd0cc461d270dbb7ea59ab4de
Julia Be <juliabaraniecka@gmail.com>
2023-03-07 21:03:12 +0100

    [add] - CarDao created

---
+++ src/main/java/pl/sda/orange/dao/CarDao.java
@@ -0,0 +1,24 @@
+package pl.sda.orange.dao;
+
+import pl.sda.orange.entity.Car;
+
+import java.util.List;
+
+public class CarDao {
+
+    public void save(Car car){
+
+    }
+
+    public List<Car> findAll(){
+        return List.of(); //unikamy zwracania nulli
+    }
+
+    public Car findById(Long id){
+        return null;
+    }
+
+    public void deleteById(Long id){
+
+    }
+}
