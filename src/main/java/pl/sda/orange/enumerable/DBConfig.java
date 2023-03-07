6b631ae17d705dcafa5e7f248e5a898a9aa98125
Julia Be <juliabaraniecka@gmail.com>
2023-03-07 19:16:36 +0100

    [add] - DBConfig enum class for explaining difference between static final field and enum usage

---
+++ src/main/java/pl/sda/orange/enumerable/DBConfig.java
@@ -0,0 +1,20 @@
+package pl.sda.orange.enumerable;
+
+public enum DBConfig {
+
+    DB_URL("jdbc:h2:~/databases/jdbc"),
+    USER("sa"),
+    PASSWORD("sa");
+
+    private String value;
+
+    DBConfig(String value){
+        this.value = value;
+    }
+
+    public String getValue(){
+        return value;
+    }
+
+
+}
