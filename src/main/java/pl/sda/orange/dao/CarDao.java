5b57e97282fe5f972d1cdef1fb08b51e47c85f85
Julia Be <juliabaraniecka@gmail.com>
2023-03-09 19:35:27 +0100

    [change] - findAll and findById dao methods implemented

--- src/main/java/pl/sda/orange/dao/CarDao.java
+++ src/main/java/pl/sda/orange/dao/CarDao.java
@@ -2,25 +2,97 @@ package pl.sda.orange.dao;
 
 import pl.sda.orange.entity.Car;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
-public class CarDao implements DataAccess<Car, Long>{
+public class CarDao implements DataAccess<Car, Long> {
 
-    @Override
-    public void save(Car car){
+    private final Connection dbConnection;
+
+    //CREATE TABLE CARS (
+    //ID BIGINT PRIMARY KEY AUTO_INCREMENT,
+    //COLOUR VARCHAR(255),
+    //BRAND VARCHAR(255),
+    //MODEL VARCHAR(255)
+    //);
+
+    public CarDao(Connection dbConnection) {
+        this.dbConnection = dbConnection;
     }
 
     @Override
-    public List<Car> findAll(){
-        return List.of(); //unikamy zwracania nulli
+    public void save(Car car) {
     }
 
     @Override
-    public Car findById(Long id){
-        return null;
+    public List<Car> findAll() {
+        //var is the same as List<Car> result = new ArrayList<Car>();
+        var cars = new ArrayList<Car>();
+
+        //tworzymy query
+
+        //TODO: validate query
+        String allCarsQuery = """
+                SELECT ID, COLOUR, BRAND, MODEL
+                FROM CARS
+                """;
+        try {
+            Statement queryStatement = dbConnection.createStatement();
+            //tabelka z odpowiedzią z bazy danych
+            ResultSet queryResult = queryStatement.executeQuery(allCarsQuery);
+
+            while (queryResult.next()) {
+                // zapytanie po indeksach bardziej wydajne niż po nazwach
+                Long id = queryResult.getLong(1);
+                String colour = queryResult.getString(2);
+                String brand = queryResult.getString(3);
+                String model = queryResult.getString(4);
+
+                Car carFromDb = new Car(id, colour, brand, model);
+                cars.add(carFromDb);
+            }
+        } catch (SQLException e) {
+            System.out.println("Unexpected SQL exception occurred");
+            e.printStackTrace();
+        }
+
+        return cars; //unikamy zwracania nulli
     }
 
     @Override
-    public void deleteById(Long id){
+    public Car findById(Long id) {
+// w kotlinie można napisać Car? i wtedy może zwrócić nulla? albo nie?
+        Car result = null;
+        String carByIdQuery = """
+                SELECT ID, COLOUR, BRAND, MODEL
+                FROM CARS
+                WHERE ID = ? 
+                """;
+        // nie sklejamy stringów w zapytaniu sql
+
+        //prepareStatement możemy sparametryzować
+        //query statement wymaga argumentu,potem w result set już nie muszę podawać
+        try {
+           PreparedStatement queryStatement = dbConnection.prepareStatement(carByIdQuery);
+            queryStatement.setLong(1, id);
+            ResultSet queryResult = queryStatement.executeQuery();
+
+            if (queryResult.next()){
+            result = new Car(queryResult.getLong("ID"),
+                    queryResult.getString("COLOUR"),
+                    queryResult.getString("BRAND"),
+                    queryResult.getString("MODEL"));
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Unexpected SQL exception occurred");
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    @Override
+    public void deleteById(Long id) {
     }
 }
---
+++ src/main/java/pl/sda/orange/jdbc/CarDaoExample.java
@@ -0,0 +1,34 @@
+package pl.sda.orange.jdbc;
+
+import pl.sda.orange.dao.CarDao;
+import pl.sda.orange.entity.Car;
+import pl.sda.orange.jdbc.config.H2Config;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class CarDaoExample {
+    public static void main(String[] args) {
+
+        System.out.println("Car dao example");
+        System.out.println("Reading all cars from db");
+
+        try {
+            var h2Connection = DriverManager.getConnection(H2Config.DB_URL,
+                    H2Config.USER,
+                    H2Config.PASSWORD);
+
+            CarDao carDao = new CarDao(h2Connection);
+            var allCarsFromDB = carDao.findAll();
+            System.out.println("All cars from db: " + allCarsFromDB);
+
+            System.out.println("Finding car by id");
+            System.out.println("First with existing id");
+            Car existingCar = carDao.findById(1L);
+            System.out.println("Existing car: " + existingCar);
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
