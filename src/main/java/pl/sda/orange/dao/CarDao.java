8cb2faec5ff18790bb2380d548d1d8856bde5a3f
Julia Be <juliabaraniecka@gmail.com>
2023-03-09 18:03:58 +0100

    [add] - generic DataAccess interface created

--- src/main/java/pl/sda/orange/dao/CarDao.java
+++ src/main/java/pl/sda/orange/dao/CarDao.java
@@ -4,21 +4,23 @@ import pl.sda.orange.entity.Car;
 
 import java.util.List;
 
-public class CarDao {
+public class CarDao implements DataAccess<Car, Long>{
 
+    @Override
     public void save(Car car){
-
     }
 
+    @Override
     public List<Car> findAll(){
         return List.of(); //unikamy zwracania nulli
     }
 
+    @Override
     public Car findById(Long id){
         return null;
     }
 
+    @Override
     public void deleteById(Long id){
-
     }
 }
